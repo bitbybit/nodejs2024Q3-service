47addd50ab5bb47c5d6d85e1f351f549109163cf
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-11-16 21:12:26 +0100

    feat: add class-validator to dto classes

--- src/favorites/favorites.dto.ts
+++ src/favorites/favorites.dto.ts
@@ -1,3 +1,4 @@
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 import { ArtistResponseDto } from '../artist/artist.dto';
@@ -5,17 +6,22 @@ import { AlbumResponseDto } from '../album/album.dto';
 import { TrackResponseDto } from '../track/track.dto';
 
 export class FavoritesResponseDto {
+  @IsArray()
   @ApiProperty({ type: [ArtistResponseDto] })
   artists: ArtistResponseDto[];
 
+  @IsArray()
   @ApiProperty({ type: [AlbumResponseDto] })
   albums: AlbumResponseDto[];
 
+  @IsArray()
   @ApiProperty({ type: [TrackResponseDto] })
   tracks: TrackResponseDto[];
 }
 
 export class FavoritesAddedResponseDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   message: string;
 }
